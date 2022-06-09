7dc5db8dd08e40de1ceae8c29cad2d2a304470c9
vikas aggarwal <vikas.aggarwal@alliancerxwp.com>
2022-06-09 17:53:58 -0500

    First Commit

---
+++ src/main/java/com/kubernetes/example/kubernetesexample/KubernetesexampleApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetes.example.kubernetesexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubernetes/example/kubernetesexample/KubernetesexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetes.example.kubernetesexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
